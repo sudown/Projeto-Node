83fdfe94f67dae501a479603d0491c7ccde06ca0
LinWithoutLau <mirosmarsantoss@gmail.com>
2022-07-15 17:00:33 -0300

    Criado diretorio routes para as rotas

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const path = require('path');
 const firebase = require('./services/firebase');
 
+//arquivos das rotas
+const login = require('./routes/login');
+const register = require('./routes/register');
+const solicitarSenha = require('./routes/solicitar-senha');
+const redefinirSenha = require('./routes/redefinir-senha');
+
+
 // Criar aplicação WEB express
 const app = express();
 
@@ -15,54 +22,15 @@ app.set('view engine', 'pug');
 // Midlewares
 app.use(express.urlencoded({ extended: true })); // parsing application/x-www-form-urlencoded
 
+app.use('/login', login);
+app.use('/register', register);
+app.use('/solicitar-senha', solicitarSenha);
+app.use('/redefinir-senha', redefinirSenha);
+
 //Rotas
 app.get('/', (req, res) => {
   //res.render('index', {titulo: 'Bem vindo!'});
   res.redirect('/login')
-});
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.post('/login', (req, res) => {
-  console.log('efetuando login ...');
-  console.log(req.body);
-  firebase.signInWithEmailAndPassword(req, res);
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.post('/register', (req, res) => {
-  console.log('cadastrando usuário ...');
-  console.log(req.body);
-  firebase.createUserWithEmailAndPassword(req, res);
-});
-
-app.get('/solicitar-senha', (req, res) => {
-  res.render('forgot-password')
-});
-
-app.post('/solicitar-senha', (req, res) => {
-  console.log("post solicitar senha");
-});
-
-app.get('/redefinir-senha', (req, res) => {
-  res.render('redefinir-senha');
-});
-
-app.post('/redefinir-senha', (req, res) => {
-  console.log(req.body);
-});
-
-app.get('/tarefas', (req, res) => {
-  res.render('tarefas');
-  //firebase.exibeTarefas();
-});
-
-app.get('/html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Rodar a aplicação express na porta 3000
